65fb4b6d9c44b4088c87d0a555d292371045c9eb
FelipePilz <felipevilvertpilz@gmail.com>
2021-12-16 10:17:24 -0300

    Testando o clique na diretiva action

--- src/app/shared/directives/action/action.directive.spec.ts
+++ src/app/shared/directives/action/action.directive.spec.ts
@@ -23,7 +23,17 @@ describe(ActionDirective.name, () => {
       fixture.nativeElement.querySelector('.dummy-component');
     const event = new KeyboardEvent('keyup', { key: 'Enter' });
     divEl.dispatchEvent(event);
-    expect(component.hasEvent()).toBe(true);
+    expect(component.hasEvent()).toBeTrue();
+  });
+
+  it(`(D) (@OutPut appAction)
+  should emit event with payload
+  when clicked`, () => {
+    const divEl: HTMLElement =
+      fixture.nativeElement.querySelector('.dummy-component');
+    const event = new Event('click');
+    divEl.dispatchEvent(event);
+    expect(component.hasEvent()).toBeTrue();
   });
 });
 
