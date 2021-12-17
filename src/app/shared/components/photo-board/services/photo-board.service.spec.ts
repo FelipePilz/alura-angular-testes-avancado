1c984d581b84adec78b3e4b2443e844699f30e05
FelipePilz <felipevilvertpilz@gmail.com>
2021-12-17 12:23:05 -0300

    Testando chamadas nas APIs

--- src/app/shared/components/photo-board/services/photo-board.service.spec.ts
+++ src/app/shared/components/photo-board/services/photo-board.service.spec.ts
@@ -5,6 +5,22 @@ import {
   HttpTestingController,
 } from '@angular/common/http/testing';
 
+const mockData = {
+  api: 'http://localhost:3000/photos',
+  data: [
+    {
+      id: 1,
+      description: 'example 1',
+      src: '',
+    },
+    {
+      id: 2,
+      description: 'example 2',
+      src: '',
+    },
+  ],
+};
+
 describe(PhotoBoardService.name, () => {
   let service: PhotoBoardService;
   let httpController: HttpTestingController;
@@ -17,5 +33,16 @@ describe(PhotoBoardService.name, () => {
 
     service = TestBed.inject(PhotoBoardService);
     httpController = TestBed.inject(HttpTestingController);
+  });
+
+  it(`#${PhotoBoardService.prototype.getPhotos.name}
+  should return list of photos
+  with description in uppercase`, (done) => {
+    service.getPhotos().subscribe((photos) => {
+      expect(photos[0].description).toBe('EXAMPLE 1');
+      expect(photos[1].description).toBe('EXAMPLE 2');
+      done();
+    });
+    httpController.expectOne(mockData.api).flush(mockData.data);
   });
 });
