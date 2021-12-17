3ad8684a1eef52461145d27cc27a3e11d197821a
FelipePilz <felipevilvertpilz@gmail.com>
2021-12-17 10:24:32 -0300

    Aplicando correção do teste com OnChanges

--- src/app/shared/components/photo-board/photo-board.component.spec.ts
+++ src/app/shared/components/photo-board/photo-board.component.spec.ts
@@ -2,6 +2,7 @@ import { Photo } from './interfaces/photo';
 import { PhotoBoardModule } from './photo-board.module';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PhotoBoardComponent } from './photo-board.component';
+import { SimpleChange, SimpleChanges } from '@angular/core';
 
 function buildPhotoList(): Photo[] {
   const photos: Photo[] = [];
@@ -32,6 +33,10 @@ describe(PhotoBoardComponent.name, () => {
   when (@Input photos) has value`, () => {
     component.photos = buildPhotoList();
     fixture.detectChanges();
+    const change: SimpleChanges = {
+      photos: new SimpleChange([], component.photos, true),
+    };
+    component.ngOnChanges(change);
     expect(component.rows.length).withContext('Number of rows').toBe(2);
     expect(component.rows[0].length)
       .withContext('Number of columns from the first row')
