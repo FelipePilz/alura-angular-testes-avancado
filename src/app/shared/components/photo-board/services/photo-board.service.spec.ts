af30d2f0ac4e16f7c1a66d8184ee626b372015d4
FelipePilz <felipevilvertpilz@gmail.com>
2021-12-17 12:12:23 -0300

    Criando o teste do serviço de photo-board

---
+++ src/app/shared/components/photo-board/services/photo-board.service.spec.ts
@@ -0,0 +1,21 @@
+import { TestBed } from '@angular/core/testing';
+import { PhotoBoardService } from 'src/app/shared/components/photo-board/services/photo-board.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+
+describe(PhotoBoardService.name, () => {
+  let service: PhotoBoardService;
+  let httpController: HttpTestingController;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PhotoBoardService],
+    });
+
+    service = TestBed.inject(PhotoBoardService);
+    httpController = TestBed.inject(HttpTestingController);
+  });
+});
