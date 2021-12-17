f2596eda4096d996983e0bc9a9712ba6951f01c9
FelipePilz <felipevilvertpilz@gmail.com>
2021-12-17 10:04:32 -0300

    Implementando o photoboard na aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,15 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotoBoardModule } from './shared/components/photo-board/photo-board.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PhotoFrameModule,
     HttpClientModule,
+    PhotoBoardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
