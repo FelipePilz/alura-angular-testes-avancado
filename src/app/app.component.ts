d0dd32cca4734fec31c35630efe9c4c23848a65b
FelipePilz <felipevilvertpilz@gmail.com>
2021-12-17 10:45:28 -0300

    Adicionando rota e movendo tudo do app para photo-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,8 @@
-import { Photo } from './shared/components/photo-board/interfaces/photo';
-import { Observable } from 'rxjs';
-import { PhotoBoardService } from './shared/components/photo-board/services/photo-board.service';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  title = 'Angular testing';
-  public photos$: Observable<Photo[]>;
-
-  constructor(private service: PhotoBoardService) {}
-
-  ngOnInit(): void {
-    this.photos$ = this.service.getPhotos();
-  }
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
-import { PhotoFrameModule } from './shared/components/photo-frame/photo-frame.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PhotoBoardModule } from './shared/components/photo-board/photo-board.module';
+import { PhotoListModule } from './components/photo-list/photo-list.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    PhotoBoardModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, PhotoListModule],
   providers: [],
   bootstrap: [AppComponent],
 })
